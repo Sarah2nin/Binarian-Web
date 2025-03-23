84a2f71fa2407b325c77060a0c599fd5f2440995
Sarahtonin <SarahCreamberry@hotmail.com>
2025-03-23 13:57:50 +0100

    Made things prettier.
    Also made a barebones version of the website, just so it looks fine in the main branch.

--- MainScript.js
+++ MainScript.js
@@ -12,3 +12,8 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>. */
+
+document.getElementById("ArtiBox").style.height = (document.getElementById("MainBox").offsetHeight)-(document.getElementById("HeadBox").offsetHeight)-(document.getElementById("FootBox").offsetHeight)
+
+
+console.log(document.getElementById("MainBox").offsetHeight)-(document.getElementById("HeadBox").offsetHeight)-(document.getElementById("FootBox").offsetHeight)
